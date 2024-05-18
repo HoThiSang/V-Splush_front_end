004c0f33f2c091bc0e534838e57292792e0f1c53
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-18 09:18:58 +0700

    feat: build card item and button component refer to ST-139

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,9 +1,9 @@
 import Button from "./Button";
-function CardItem({title,text}) {
+function CardItem({title,text,link}) {
   return (
     <>
       <div className="card" style={{width:  '20rem'}}>
-          <img src="https://res.cloudinary.com/dt8km1sxl/image/upload/v1710518102/V-Splush/makeup_sp2.2_fawzgp.webp" className="card-img-top" alt="..." style={{width:'100%',height:'200px'}}/>
+          <img src={link} className="card-img-top" alt="..." style={{width:'100%',height:'200px'}}/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text" style={{width:'100%'}}>{text}</p>
