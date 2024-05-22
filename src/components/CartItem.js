534dfeb6c1f616e0a91e86cd44bda43f0cb3bca6
thisang <sang.ho25@student.passerellesnumeriques.org>
2024-05-22 14:28:56 +0700

    feat: delete cart by id refer to ST-189

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,46 @@
-function CartItem(cart ){
-    const { image_url, product_name,  discount, total_price , quantity}  = cart;
-   
-    return (
-        <tr>
-          <th scope="row"><i className="fa-solid fa-heart"></i></th>
-          <td ><img src={image_url} alt={product_name}  style={{ width: '160px'}}/></td>
-          <td>{product_name}</td>
-          <td>{total_price}</td>
-          <td>{ discount }</td>
-          <td>{quantity}</td>
-          <td><i class="fa-solid fa-cart-shopping"></i></td>
-        </tr>
-    )
+import { Button, Checkbox } from "antd";
+import React from "react";
+
+function CartItem(cart) {
+  const {
+    id,
+    image_url,
+    product_name,
+    discount,
+    total_price,
+    quantity,
+    handleDeletCart,
+  } = cart;
+
+
+  return (
+    <tr key={id}>
+      <th>
+        <Checkbox
+          className="cart-checkbox"
+          // checked={selectedItems.includes(cart.id)}
+          // onChange={() => handleCheckboxChange(cart.id)}
+        />
+      </th>
+      <td>
+        <img src={image_url} alt={product_name} style={{ width: "160px" }} />
+      </td>
+      <td>{product_name}</td>
+      <td>{total_price}</td>
+      <td>{discount}</td>
+      <td>{quantity}</td>
+      <td>
+        <Button
+          type="primary"
+          shape="round"
+          size="middle"
+          onClick={() => handleDeletCart(id)}
+        >
+          <i className="fa-solid fa-trash"></i>
+        </Button>
+      </td>
+    </tr>
+  );
 }
 
 export default CartItem;
