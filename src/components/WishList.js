90e7448466c22197f76ce9619f42015bd019ac6b
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-05-22 20:43:16 +0700

    feat: show all wish list refer to ST-148

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function WishlistItem({ index, image_url, product_name, category, price }) {
+function WishlistItem({ image_url, product_name, category_name, price }) {
   return (
     <tr>
-      <th scope="row">{index + 1}</th>
+      <th scope="row"><i className="fa-solid fa-heart"></i></th>
       <td><img src={image_url} alt={product_name} style={{ width: '160px' }} /></td>
       <td>{product_name}</td>
-      <td>{category}</td>
+      <td>{category_name}</td>
       <td>{price}</td>
       <td><i className="fa-solid fa-trash"></i></td>
     </tr>
