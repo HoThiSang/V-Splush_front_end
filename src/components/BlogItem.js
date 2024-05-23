b0426bf0e66eea3e7509961c8c8617eb1b6fcde6
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-23 23:26:44 +0700

    feat: build UI blog page refer to ST-144

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom";
 function BlogItem({props}) {
   const { id, title, content, image_url } = props;
   return (
-    <div className="card" style={{marginLeft:'30px'}}>
-      <Link to={`/blog-detail/${id}`} component={'BlogDteail'}>
-        <img src={image_url} className="card-img-top blog-img" alt="..." />
+    <div className="card card-blog">
+      <Link to={`/blog-detail/${id}`}>
+        <img src={image_url} className="card-img-top card-img-blog" alt="..." />
       </Link>
-      <div className="card-body" style={{backgroundColor:'rgba(237, 245, 247, 1)'}}>
-        <h5 className="card-title blog-content">{title}</h5>
-        <p className="card-text blog-content">{content}</p>
+      <div className="card-body card-body-blog" >
+        <h5 className="card-title card-title-blog">{title}</h5>
+        <p className="card-text card-title-blog">{content}</p>
       </div>
     </div>
   );
 }
 
+
+
 export default BlogItem;
+
+
