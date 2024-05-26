f400f5b90c6d6dc4ae7a429f99a9aeae16fea866
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-26 14:06:37 +0700

    feat: build UI blog page refer to ST-144

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import { Routes, Route}  from 'react-router-dom'
-import { Home, Cart, Test, WishList } from './pages/users'
+import { Home, Cart, Test, WishList,Blog } from './pages/users'
 import About from './components/About.js';
-import {Blog} from './pages/users/Blog.js';
-import {Blog, BlogDetail} from './pages/users/Blog.js';
-import {Home , Cart, Test,Blog} from './pages/users'
 
 function App() {
  
@@ -17,6 +14,8 @@ function App() {
         <Route path='/'  element={<Home />} />
         <Route path='/carts'  element={<Cart />} />
         <Route path='/test'  element={<Test />} />
+        <Route path='/wishlist' element={<WishList />} />
+        <Route path='/about'  element={<About />} />
         <Route path='/blog' element={<Blog />} />
       </Routes>
       <Footer />
