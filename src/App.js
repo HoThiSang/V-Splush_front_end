863566b274fd7c659fa0755e6277c6be072786f0
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-27 21:17:39 +0700

    feat: build UI blog page refer to ST-144

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import Header from './layouts/Header.js';
 import Footer from './layouts/Footer.js';
 import { Routes, Route}  from 'react-router-dom'
-import { Home, Cart, Test, WishList, HomeTest } from './pages/users'
-import { Home, Cart, Test, WishList,Blog } from './pages/users'
+import { Home, Cart, Test, WishList, HomeTest,Blog } from './pages/users'
 import About from './components/About.js';
 
 function App() {
