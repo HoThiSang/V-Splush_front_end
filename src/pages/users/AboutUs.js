d6bfeab8aeed6ac37200de1a38d92d93401fad1d
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-27 21:13:02 +0700

    Test

---
+++ src/pages/users/AboutUs.js
@@ -0,0 +1,131 @@
+import { Alert } from "antd";
+
+function AboutUs() {
+  return (
+    <>
+      <Alert
+        message="MISSION"
+        description="An About Us page is a section on a website that provides information about a company, organization, or individual. It is an opportunity to tell your brand’s story, share your vision, history, values, and achievements, and introduce team members. This is where you build trust and credibility with customers."
+        type="info"
+        className="alert-about-us"
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2"></div>
+          <div className="col-lg-10">
+            <div className="col-lg-8">
+              <h2>The work values we thrive for</h2>
+              <div className="col-lg-1">
+                <i class="fa fa-user fa-2x" aria-hidden="true" />
+              </div>
+              <div className="col-lg-7">
+                <h5>Beauty Experts</h5>
+                <p>
+                  The majority have suffered alteration in some form, buying to
+                  injected humour, or randomised words which desktop publishing
+                  packages.
+                </p>
+                <hr />
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <div className="col-lg-1">
+                <i class="fa fa-user fa-2x" aria-hidden="true" />
+              </div>
+              <div className="col-lg-7">
+                <h5>Great Services</h5>
+                <p>
+                  The majority have suffered alteration in some form, buying to
+                  injected humour, or randomised words which desktop publishing
+                  packages.
+                </p>
+                <hr />
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <div className="col-lg-1">
+                <i class="fa fa-user fa-2x" aria-hidden="true" />
+              </div>
+              <div className="col-lg-7">
+                <h5>100% Genuine</h5>
+                <p>
+                  The majority have suffered alteration in some form, buying to
+                  injected humour, or randomised words which desktop publishing
+                  packages.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">   
+            <div className="col-lg-4">
+              <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+            <h2>What our Customers says...</h2>
+            <div className="col-lg-12">
+            <div className="col-lg-6">
+            <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+            <div className="col-6">
+            <h5>100% Genuine</h5>
+              <p>
+                The majority have suffered alteration in some form, buying to
+                injected humour, or randomised words which desktop publishing
+                packages.
+              </p>
+            </div>
+            </div>
+            
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AboutUs;
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -4,3 +4,10 @@ export { default as Test } from "./Test";
 export { default as WishList } from "./WishList";
 export {default as Blog} from "./Blog";
 export {default as Product } from './Product'
+
+export {default as BlogDetail}  from "./BlogDetail"
+export {default as ContactUs}  from "./ContactUs"
+export {default as AboutUs}  from "./AboutUs"
+
+
+
