461a40b744479602edce4db86f53c74164cb132a
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-05-27 22:43:14 +0700

    view api

--- src/pages/users/Home.js
+++ src/pages/users/Home.js
@@ -2,6 +2,8 @@ import { React, useState, useEffect } from "react";
 import { Bennefit, Location, About, CardItem } from "../../components";
 import { BennefitData } from "../../data";
 import { Banner } from "../../layouts";
+import axios from "axios";
+// import axiosService from "../../services/configAxios";
 import "./style.css";
 import { Card } from "antd";
 import axiosService from "../../services/configAxios";
@@ -13,7 +15,9 @@ function Home() {
 
   const fetchData = async () => {
     try {
-      const response = await axiosService.get("/admin-product");
+      const response = await axiosService.get(
+        "/admin-product"
+      );
       setPopularProducts(response.data.data);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -29,7 +33,9 @@ function Home() {
 
   const fechDataBanner = async () => {
     try {
-      const response = await axiosService.get("/admin-show-all-banner");
+      const response = await axiosService.get(
+        "/admin-show-all-banner"
+      );
       console.log(response.data);
       setBanners(response.data.data);
     } catch (error) {
@@ -42,19 +48,20 @@ function Home() {
   }, []);
   console.log(banners);
 
+
   return (
     <>
-      <div className="body-content outer-top-xs" id="top-banner-and-menu">
-        <div className="container">
-          <div className="row">
-            <div
-              className="container"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
+    <div className="body-content outer-top-xs" id="top-banner-and-menu">
+      <div className="container">
+        <div className="row">
+          <div className="container"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+          
               <div className="col-xs-12 col-sm-12 col-md-9 homebanner-holder ">
                 <div id="hero">
                   <div
@@ -114,26 +121,27 @@ function Home() {
                     </div>
                   </div>
                 </div>
-                <div
-                  id="product-tabs-slider"
-                  className="scroll-tabs outer-top-vs wow fadeInUp mb-5"
+                <div id="product-tabs-slider" className="scroll-tabs outer-top-vs wow fadeInUp mb-5"
                 >
-                  {displayedProducts.map((item, index) => (
-                    <CardItem
-                      key={item.id}
-                      product_name={item.product_name}
-                      description={item.description}
-                      image_url={item.image_url}
-                    />
+                       { displayedProducts.map((item, index) => (
+                        <CardItem 
+                          key={item.id} 
+                          product_name={item.product_name}
+                          description={item.description}
+                          image_url={item.image_url}
+                        />
                   ))}
                 </div>
-              </div>
             </div>
           </div>
           <About />
         </div>
+        <About />
+      
       </div>
-      <Location />
+     
+    </div>
+    <Location />
     </>
   );
 }
