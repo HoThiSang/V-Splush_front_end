1b86eb9343f4cb7eaba5bb8c2b9cc09009b19a2f
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-05-27 22:15:12 +0700

    Create product detail

--- src/pages/users/ProductDetail.js
+++ src/pages/users/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom'; // Import useParams
+import { Link, useParams } from 'react-router-dom';
 import axiosService from '../../services/configAxios';
 import Button from '../../components/Button';
 
@@ -7,8 +7,6 @@ const ProductDetail = () => {
   const [product, setProduct] = useState(null);
   const [images, setImages] = useState([]);
   const [mainImage, setMainImage] = useState('');
-  const [error, setError] = useState(null);
-
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,78 +14,68 @@ const ProductDetail = () => {
       try {
         const response = await axiosService.get(`/admin-product-detail/${id}`);
         const data = response.data;
-        console.log(data)
-        if (data.status === 'success') {
-          setProduct(data.productDetail);
-          setImages(data.imageAll);
-          setMainImage(data.imageAll[0]);
-        } else {
-          setError('Product not found');
-        }
+        console.log(data);
+        setProduct(data.productDetail);
+        setImages(data.imageAll);
+        setMainImage(data.imageAll[0]);
       } catch (error) {
-        setError('Error fetching product data');
         console.error('Error fetching product data:', error);
       }
     };
 
     fetchProduct();
-  }, [id]); 
+  }, [id]);
 
   const handleThumbnailClick = (image) => {
     setMainImage(image);
   };
 
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  if (!product) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="container">
-      <div className="product-detail row">
-        <div className="left-section col-6">
-          <div className="row image1">
-            <img src={mainImage.image_url} alt="Main Product" className="main-image" />
+      {product && (
+        <div className="product-detail row">
+          <div className="left-section col-6">
+            <div className="row image1">
+              <img src={mainImage.image_url} alt="Main Product" className="main-image" />
+            </div>
+            <div className="row border">
+              {images.map((image, index) => (
+                <div className='col-md-4 border' key={image.id || index}>
+                  <img
+                    src={image.image_url}
+                    alt={`Thumbnail ${index + 1}`}
+                    className="thumbnail"
+                    onClick={() => handleThumbnailClick(image)}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="thumbnails">
+            </div>
           </div>
-          <div className="row border">
-            {images.map((image, index) => (
-              <div className='col-md-4 border'>
-                <img
-                  key={index}
-                  src={image.image_url}
-                  alt={`Thumbnail ${index + 1}`}
-                  className="thumbnail"
-                  onClick={() => handleThumbnailClick(image)}
-                />
-              </div>
-            ))}
-          </div>
-          <div className="thumbnails">
+          <div className="right-section col-6">
+            <h1 className="product-title">{product.product_name}</h1>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">${product.price}</p>
+            <hr />
+            <div className="add-to-cart">
+              <Link to="/">
+                <Button className="btn-outline-success" width="600px" title="Add to cart" color="#abd07e" />
+              </Link>
+            </div>
+            <div className="buy-now">
+              <Button className="btn-outline-success" width={"600px"} href="#" title="Buy now" color={"#abd07e"} />
+            </div>
           </div>
         </div>
-        <div className="right-section col-6">
-          <h1 className="product-title">{product.product_name}</h1>
-          <p className="product-description">{product.description}</p>
-          <p className="product-price">${product.price}</p>
+      )}
+      {product && (
+        <div className="ingredient">
+          <h3>Ingredient</h3>
           <hr />
-          <div className="add-to-cart">
-            <Link to="/">
-              <Button className="btn-outline-success" width="600px" title="Add to cart" color="#abd07e" />
-            </Link>
-          </div>
-          <div className="buy-now">
-            <Button className="btn-outline-success" width={"600px"} href="#" title="Buy now" color={"#abd07e"} />
-          </div>
+          <p className="product-description">{product.ingredient}</p>
         </div>
-      </div>
-      <div className="ingredient">
-        <h3>Ingredient</h3>
-        <hr/>
-        <p className="product-description">{product.ingredient}</p>
-      </div>
+      )}
     </div>
   );
 };
