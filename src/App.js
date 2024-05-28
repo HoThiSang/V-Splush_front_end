6e65509494b7372283ff69faf7c8b9bbd310ee5b
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-28 15:51:14 +0700

    feat: build blog detail page  refer to ST-145

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './layouts/Header.js';
 import Footer from './layouts/Footer.js';
 import { Routes, Route}  from 'react-router-dom'
-import { Home, Cart, Test, WishList,Blog } from './pages/users'
+import { Home, Cart, Test, WishList,Blog,BlogDetail} from './pages/users'
 import About from './components/About.js';
 import Product from './pages/users/Product.js';
 import RegistrationForm from './pages/auth/RegisterUser.js';
@@ -22,6 +22,9 @@ function App() {
         <Route path='/register'  element={<RegistrationForm />} />
         <Route path='/login'  element={<Login />} />
       </Routes>
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/blog-detail/:id' element={<BlogDetail />} />
+    </Routes>
       <Footer />
     </div>
   );
---
+++ src/pages/users/BlogDetail.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axiosService from "../../services/configAxios";
+import { Alert } from "antd";
+import { useParams } from "react-router";
+
+function BlogDetail() {
+  const { id } = useParams();
+  const [blog, setBlog] = useState({});
+
+  useEffect(() => {
+    const getBlogDetail = async () => {
+      try {
+        const res = await axiosService.get(`/admin-show-post/${id}`);
+        setBlog(res.data.data);
+      } catch (error) {
+        console.error("Error fetching blog data:", error);
+      }
+    };
+    getBlogDetail();
+  }, []);
+
+
+  return (
+    <>
+      <Alert
+        description="Bank your beauty sleep with our overnight skin care edit"
+        type="info"
+        className="alert-blog-detail"
+      />
+      <div className="div-blog-detail">
+        <img
+          className="img-blog-detail"
+          src={blog.image_url}
+          alt={blog.title}
+        />
+        <p className="p-blog-content"><span>{blog.content}</span></p>
+      </div>
+    </>
+  );
+}
+
+export default BlogDetail;
